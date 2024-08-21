6907c8a48efcac932bea31d85d0e44f816b10897
esfuerzo <dkdldrha@naver.com>
2024-08-21 16:04:55 +0900

    first commit

---
+++ src/main/java/com/fconlineserverv1/FcOnlineServerV1Application.java
@@ -0,0 +1,13 @@
+package com.fconlineserverv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcOnlineServerV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcOnlineServerV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fconlineserverv1/FcOnlineServerV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fconlineserverv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcOnlineServerV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
